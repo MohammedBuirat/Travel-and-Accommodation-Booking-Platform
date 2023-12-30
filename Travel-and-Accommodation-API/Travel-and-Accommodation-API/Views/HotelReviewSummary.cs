9221d67a3af9d731f744f0383cc390d49a3761ce
Mohammed Buirat <1192896@student.birzeit.edu>
2023-12-30 21:12:50 +0200

    Building Views

---
+++ Travel-and-Accommodation-API/Travel-and-Accommodation-API/Views/HotelReviewSummary.cs
@@ -0,0 +1,16 @@
+﻿namespace Travel_and_Accommodation_API.Views
+{
+    public class HotelReviewSummary
+    {
+        public Guid HotelId { get; set; }
+        public int NumOfReviews { get; set; }
+        public decimal OverAllRatings { get; set; }
+        public decimal SecurityRating { get; set; }
+        public decimal StaffRating { get; set; }
+        public decimal CleanlinessRating { get; set; }
+        public decimal ValueForMoneyRating { get; set; }
+        public decimal LocationRating { get; set; }
+        public decimal AtmosphereRating { get; set; }
+        public decimal FacilitiesRating { get; set; }
+    }
+}
